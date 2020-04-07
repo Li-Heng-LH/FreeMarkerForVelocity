4469ab87efd5da64c401cf13e9a89b84e0b7884b
Li Heng <liheng6567@gmail.com>
2020-04-07 15:20:46 +0800

    Velocity poc to generate java class

--- TryVelocity/src/main/java/me/liheng/codeGenerator/Attribute.java
+++ TryVelocity/src/main/java/me/liheng/codeGenerator/Attribute.java
@@ -1,5 +1,34 @@
 package me.liheng.codeGenerator;
 
+import org.apache.velocity.util.StringUtils;
+
 public class Attribute {
 
+    private String attributeName;
+    private String attributeType;
+
+    public Attribute(String attributeName, String attributeType) {
+        this.attributeName = attributeName;
+        this.attributeType = attributeType;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    public String getAttributeType() {
+        return attributeType;
+    }
+
+    public void setAttributeType(String attributeType) {
+        this.attributeType = attributeType;
+    }
+
+    public String getGetterAndSetterString() {
+        return StringUtils.capitalizeFirstLetter(attributeName);
+    }
 }
--- TryVelocity/src/main/java/me/liheng/codeGenerator/VelocityWriter.java
+++ TryVelocity/src/main/java/me/liheng/codeGenerator/VelocityWriter.java
@@ -10,4 +10,36 @@ import java.util.List;
 
 public class VelocityWriter {
 
+    static String className = "User";
+    static String packageName = "me.liheng.project";
+
+    public static void main(String[] args) {
+
+        //Initialise the velocity engine
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.init();
+
+        //Read the template
+        Template template = velocityEngine.getTemplate("/src/main/resources/vtemplates/class.vm");
+
+        //Initialise the velocity context
+        VelocityContext context = new VelocityContext();
+
+        //Put the data model in context object
+        context.put("packageName", packageName);
+        context.put("className", className);
+        List<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("id", "int"));
+        attributes.add(new Attribute("firstName", "String"));
+        attributes.add(new Attribute("lastName", "String"));
+        attributes.add(new Attribute("dob", "LocalDate"));
+        context.put("attributes", attributes);
+
+        //Merge the template with context data and render the view
+        StringWriter writer = new StringWriter();
+        template.merge(context, writer);
+
+        System.out.println(writer.toString());
+    }
+
 }
