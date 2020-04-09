d380259f703f6054b9c9841c383dcaf6ae0b6fc6
Li Heng <liheng6567@gmail.com>
2020-04-09 11:47:35 +0800

    Send email with attachment

--- VelocityMail/src/main/java/me/liheng/EmailWithVelocity.java
+++ VelocityMail/src/main/java/me/liheng/EmailWithVelocity.java
@@ -1,8 +1,13 @@
 package me.liheng;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 public class EmailWithVelocity {
@@ -24,29 +29,59 @@ public class EmailWithVelocity {
         Session session = Session.getDefaultInstance(properties,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(from, "16 digit app password");
+                        return new PasswordAuthentication(from, "16 digit");
                     }
                 });
 
         try {
+
             //Create a default MimeMessage object
             MimeMessage message = new MimeMessage(session);
-
             //Set To: header field of the header
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
             //Set Subject: header field
             message.setSubject("This is the Subject Line!");
 
-            //Now set the actual message
-            message.setText("This is actual message");
-
-            //Send message
-            Transport.send(message);
+            emailWithAttachment(message);
             System.out.println("Sent message successfully....");
 
         } catch (MessagingException mex) {
             mex.printStackTrace();
         }
+    }
+
+
+    //simple email
+    private static void emailWithText(MimeMessage message) throws MessagingException {
+        //set the actual message
+        message.setText("This is actual message");
+        //send message
+        Transport.send(message);
+    }
+
+
+    //email with attachment
+    private static void emailWithAttachment(MimeMessage message) throws MessagingException {
+        //create MimeBodyPart object and set message text
+        BodyPart messageBodyPart1 = new MimeBodyPart();
+        messageBodyPart1.setText("This is the message body!\n");
+
+        //create new MimeBodyPart object and set DataHandler object to MimeBodyPart
+        MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+        String filename = "src/main/resources/attachments/a.txt";
+        DataSource source = new FileDataSource(filename);
+        messageBodyPart2.setDataHandler(new DataHandler(source));
+        messageBodyPart2.setFileName("a.txt");
+
+        //create Multipart object and add MimeBodyPart objects to this object
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart1);
+        multipart.addBodyPart(messageBodyPart2);
+
+        //set the multiplart object to the message object
+        message.setContent(multipart);
+
+        //send message
+        Transport.send(message);
     }
 }
