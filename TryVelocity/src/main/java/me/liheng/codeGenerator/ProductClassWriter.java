cdd7af503411b109fe80be0ffd1a89e82bcb95b6
Li Heng <liheng6567@gmail.com>
2020-04-14 11:29:11 +0800

    Refactor class writer for testing of SIngleton

---
+++ TryVelocity/src/main/java/me/liheng/codeGenerator/ProductClassWriter.java
@@ -0,0 +1,15 @@
+package me.liheng.codeGenerator;
+
+import org.apache.velocity.VelocityContext;
+
+import java.io.IOException;
+
+public class ProductClassWriter {
+
+    public static void write() throws IOException {
+
+        //Initialise the velocity context
+        VelocityContext context = new VelocityContext();
+
+    }
+}
--- TryVelocity/src/main/java/me/liheng/codeGenerator/SingletonVelocityWriter.java
+++ TryVelocity/src/main/java/me/liheng/codeGenerator/SingletonVelocityWriter.java
@@ -4,42 +4,19 @@ import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
+import java.io.IOException;
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 public class SingletonVelocityWriter {
 
-    static String className = "User";
-    static String packageName = "me.liheng.project";
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         initialiseVelocity2();
 
-        //Read the template USING STATIC method
-        Template template = Velocity.getTemplate("class.vm");
-
-        //Initialise the velocity context
-        VelocityContext context = new VelocityContext();
-
-        //Put the data model in context object
-        context.put("packageName", packageName);
-        context.put("className", className);
-        List<Attribute> attributes = new ArrayList<>();
-        attributes.add(new Attribute("id", "int"));
-        attributes.add(new Attribute("firstName", "String"));
-        attributes.add(new Attribute("lastName", "String"));
-        attributes.add(new Attribute("dob", "LocalDate"));
-        context.put("attributes", attributes);
-
-        //Merge the template with context data and render the view
-        StringWriter writer = new StringWriter();
-        template.merge(context, writer);
-
-        System.out.println(writer.toString());
+        UserClassWriter.write();
     }
+
 
     private static void initialiseVelocity1() {
         Properties properties = new Properties();
